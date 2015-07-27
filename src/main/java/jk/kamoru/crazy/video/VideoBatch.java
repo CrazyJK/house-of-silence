c5c5975474d04c41820b2e79ee86d197fafbbdeb
kamoru <kamoru@kamoru-home>
2015-07-27 23:05:04 +0900

    add method properties show switch log level of springframework to warn

--- src/main/java/jk/kamoru/crazy/video/VideoBatch.java
+++ src/main/java/jk/kamoru/crazy/video/VideoBatch.java
@@ -1,5 +1,7 @@
 package jk.kamoru.crazy.video;
 
+import javax.annotation.PostConstruct;
+
 import jk.kamoru.crazy.video.service.VideoService;
 
 import org.slf4j.Logger;
@@ -16,30 +18,40 @@ public class VideoBatch {
 
 	@Autowired VideoService videoService;
 
-	@Value("#{prop['batch.watched.moveVideo']}")	private static boolean MOVE_WATCHED_VIDEO;
-	@Value("#{prop['batch.rank.deleteVideo']}") 	private static boolean DELETE_LOWER_RANK_VIDEO;
-	@Value("#{prop['batch.score.deleteVideo']}") 	private static boolean DELETE_LOWER_SCORE_VIDEO;
+	@Value("#{prop['batch.watched.moveVideo']}")	private boolean MOVE_WATCHED_VIDEO;
+	@Value("#{prop['batch.rank.deleteVideo']}") 	private boolean DELETE_LOWER_RANK_VIDEO;
+	@Value("#{prop['batch.score.deleteVideo']}") 	private boolean DELETE_LOWER_SCORE_VIDEO;
 
 	public boolean isMOVE_WATCHED_VIDEO() {
 		return MOVE_WATCHED_VIDEO;
 	}
 	public void setMOVE_WATCHED_VIDEO(boolean setValue) {
 		MOVE_WATCHED_VIDEO = setValue;
-		logger.info("set {}", MOVE_WATCHED_VIDEO);
+		showProperties();
 	}
 	public boolean isDELETE_LOWER_RANK_VIDEO() {
 		return DELETE_LOWER_RANK_VIDEO;
 	}
 	public void setDELETE_LOWER_RANK_VIDEO(boolean setValue) {
 		DELETE_LOWER_RANK_VIDEO = setValue;
+		showProperties();
 	}
 	public boolean isDELETE_LOWER_SCORE_VIDEO() {
 		return DELETE_LOWER_SCORE_VIDEO;
 	}
 	public void setDELETE_LOWER_SCORE_VIDEO(boolean setValue) {
 		DELETE_LOWER_SCORE_VIDEO = setValue;
+		showProperties();
 	}
 
+	@PostConstruct
+	private void showProperties() {
+		logger.info("Batch properties");
+		logger.info("  - batch.watched.moveVideo = {}", MOVE_WATCHED_VIDEO);
+		logger.info("  - batch.rank.deleteVideo  = {}", DELETE_LOWER_RANK_VIDEO);
+		logger.info("  - batch.score.deleteVideo = {}", DELETE_LOWER_SCORE_VIDEO);
+	}
+	
 	@Scheduled(cron="0 */5 * * * *")
 	public synchronized void batchVideoSource() {
 		
